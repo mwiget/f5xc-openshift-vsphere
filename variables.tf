2bcc01ccc8552f8874ec748d1d58558f98da6230
Marcel Wiget <mwiget@gmail.com>
2022-07-24 12:01:21 +0000

    wip

--- variables.tf
+++ variables.tf
@@ -6,22 +6,17 @@ variable "projectPrefix" {
 variable "api_url" {
   description = "url of volterra api"
   type        = string
-  default     = ""
-}
-variable "api_p12" {
-  description = "p12 content base64 encoded"
-  type        = string
-  default     = ""
+#  default     = ""
 }
 variable "api_p12_file" {
   description = "Location of volterra p12 file"
   type        = string
-  default     = ""
+#  default     = ""
 }
 variable "api_ca_cert" {
   description = "public_server_ca.crt file (only required for non-production tenants)"
   type        = string
-  default     = ""
+#  default     = ""
 }
 variable "api_cert" {
   type        = string
@@ -31,4 +26,3 @@ variable "api_key" {
   type        = string
   default     = ""
 }
-
