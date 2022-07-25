d7847a624640ac054480f60a020b8bfaeaff65a8
Marcel Wiget <mwiget@gmail.com>
2022-07-25 12:27:31 +0000

    fix

--- k.sh
+++ k.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-for i in 2 3; do
-  kubectl config use-context default/api-osc$i-mwlabs-net:6443/kube:admin
-  kubectl $@
+for cluster in osc2 osc3; do
+  kubectl --kubeconfig kc-$cluster $@
 done
